4cd0760eedbbb40ef2af24e901e4a019e807d096
Amir Samoha <amir.samoha42@gmail.com>
2023-11-21 17:45:07 +0200

    create a backend and build server + database

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGOOSE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connect success to database");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to database: ${err}`);
+  });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server runnig on http://localhost:${port}`);
+});
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  const { data } = req.body;
+
+  try {
+    if (data === "amir") {
+      res.send({ status: "success", data: data });
+    } else {
+      res.send({ status: "User not found" });
+    }
+  } catch (error) {
+    res.send({ status: "something went wrong trying again" });
+  }
+});
